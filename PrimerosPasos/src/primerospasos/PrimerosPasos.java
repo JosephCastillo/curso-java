b7b5832a098a997ffc1ae8f2d280c53bb852f4c8
JosephCastillo <1308004+JosephCastillo@users.noreply.github.com>
2025-02-03 21:14:52 -0500

    Hola mundo

---
+++ PrimerosPasos/src/primerospasos/PrimerosPasos.java
@@ -0,0 +1,13 @@
+package primerospasos;
+/**
+ *
+ * @author JosephCastillo
+ * @Date   3/02/2025
+ */
+
+public class PrimerosPasos {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
